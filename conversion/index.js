b52ddaff1fbbcf9761ef09045dabd9bef92bd0ee
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-07-05 19:46:22 +0530

    type conversion

---
+++ conversion/index.js
@@ -0,0 +1,33 @@
+let myObject = { name: "Vipul" };
+// console.log(myObject);
+
+myObject.fullName = "Vipul Patel";
+
+// console.log(myObject);
+
+let course = new Object();
+course.name = "hindi";
+// console.log(course);
+
+let x = course;
+// console.log(x);
+course.duration = 12;
+x.duration = 15;
+// console.log(course);
+// console.log(x);
+
+let lower = "lower";
+
+let U = lower.toUpperCase();
+// console.log(U);
+
+let l = U.toLowerCase();
+// console.log(l);
+
+let num = 20;
+let num2 = "jfdnld";
+// console.log(typeof num);
+// console.log(typeof num.toString());
+// console.log(Number(num2));
+
+console.log(isNaN("2"));
